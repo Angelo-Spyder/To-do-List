b9176630e0799d0bbc8c1861811c9456bb2c5531
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-03-26 16:23:03 -0300

    Criando atividades na lista de tarefas e fazendo o usuário adicionar novas atividades

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+
 var conteudoAtividades = document.querySelector(".atividades");
-console.log(conteudoAtividades);
+var botaoAdiciona = document.querySelector("#botaoAdiciona");
+var textoInput = document.querySelector("#texto");
+
+botaoAdiciona.addEventListener("click", () => {
+    var divPrincipal = document.createElement("div");
+    divPrincipal.classList.add("conteudo");
+    
+    var spanAtividade = document.createElement("span");
+    spanAtividade.textContent = textoInput.value;
+    spanAtividade.classList.add("texto-atividade");
+
+    var spanIcone = document.createElement("span")
+    spanIcone.innerHTML = `<img  src="lixeira-icon.png" alt="Lixeira de reciclagem">`;
+    spanIcone.classList.add("icone")
+
+    divPrincipal.appendChild(spanAtividade);
+    divPrincipal.appendChild(spanIcone)
+    conteudoAtividades.appendChild(divPrincipal)
+
+});
