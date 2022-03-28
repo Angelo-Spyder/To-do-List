accd6f18615812b17d6a3ffd45f5c760ca9e3ae1
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-03-28 16:56:25 -0300

    Refatorando código, dividindo funções e adicionando comentários

--- main.js
+++ main.js
@@ -1,39 +1,54 @@
 
+//Captura de dados e tags
+
 var conteudoAtividades = document.querySelector(".atividades");
-var botaoAdiciona = document.querySelector("#botaoAdiciona");
+var botaoAdicionaElementos = document.querySelector("#botaoAdiciona");
 var textoInput = document.querySelector("#texto");
 var IconeLixeira = document.querySelectorAll(".icone");
 var erro = document.querySelector(".erro");
 
-botaoAdiciona.addEventListener("click", (event) => {
+botaoAdicionaElementos.addEventListener("click", (event) => {
     
     if(textoInput.value.length == 0){  
         erro.textContent = "Insira um texto"
-
         return
     }else{
-        erro.innerHTML = ""
-
-        var divPrincipal = document.createElement("div");
-        divPrincipal.classList.add("conteudo");
-        
-        var spanAtividade = document.createElement("span");
-        spanAtividade.textContent = textoInput.value;
-        spanAtividade.classList.add("texto-atividade");
-    
-        var spanIcone = document.createElement("span")
-        spanIcone.classList.add("icone");
-        spanIcone.innerHTML = `<img  src="lixeira-icon.png" alt="Lixeira de reciclagem">`;
-        
-    
-        divPrincipal.appendChild(spanAtividade);
-        divPrincipal.appendChild(spanIcone);
+        erro.innerHTML = "";
+        var divPrincipal = criaDivPrincipal();
         conteudoAtividades.appendChild(divPrincipal);
-
-        textoInput.value = ""
+        textoInput.value = "";
     }
 
 });
+
+//Cria o elemento novo na lista
+
+function criaDivPrincipal(){
+
+    var div = document.createElement("div");
+    div.classList.add("conteudo");
+        
+    //Cria o texto da atividade
+
+    var spanAtividade = document.createElement("span");
+    spanAtividade.textContent = textoInput.value;
+    spanAtividade.classList.add("texto-atividade");
+    
+    //Cria o icone de lixeira 
+
+    var spanIcone = document.createElement("span");
+    spanIcone.classList.add("icone");
+    spanIcone.innerHTML = `<img  src="lixeira-icon.png" alt="Lixeira de reciclagem">`;
+
+    div.appendChild(spanAtividade);
+    div.appendChild(spanIcone);
+
+    return div;
+
+};
+
+
+//Apagando elementos da lista
 
 IconeLixeira.forEach(
     () => {
