0479738d328991e2caafc08010fef2d081d6b197
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-05-16 13:57:34 -0300

    Refatorando o código e adicionando local storage para guardar os dados

--- main.js
+++ main.js
@@ -7,23 +7,38 @@ var textoInput = document.querySelector("#texto");
 var IconeLixeira = document.querySelectorAll(".icone");
 var erro = document.querySelector(".erro");
 
-botaoAdicionaElementos.addEventListener("click", (event) => {
-    
-    if(textoInput.value.length == 0){  
-        erro.textContent = "Insira um texto"
-        return
-    }else{
-        erro.innerHTML = "";
-        var divPrincipal = criaDivPrincipal();
-        conteudoAtividades.appendChild(divPrincipal);
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    criaDivPrincipal(elemento)
+})
+
+botaoAdicionaElementos.addEventListener("click", function(){
+       
+        var itemAtual = {
+            "texto": textoInput.value
+        }
+        
+        var existe = itens.find( elemento => elemento.texto === itemAtual.texto)
+
+        if(existe){
+            itemAtual.id = existe.id
+        }else{
+            itemAtual.id = itens.length
+            criaDivPrincipal(itemAtual);
+            itens.push(itemAtual)
+        }
+
+        localStorage.setItem("itens", JSON.stringify(itens))
+
         textoInput.value = "";
-    }
+        erro.innerHTML = "";
 
 });
 
 //Cria o elemento novo na lista
 
-function criaDivPrincipal(){
+function criaDivPrincipal(item){
 
     var div = document.createElement("div");
     div.classList.add("conteudo");
@@ -31,36 +46,47 @@ function criaDivPrincipal(){
     //Cria o texto da atividade
 
     var spanAtividade = document.createElement("span");
-    spanAtividade.textContent = textoInput.value;
+    spanAtividade.textContent = item.texto;
     spanAtividade.classList.add("texto-atividade");
     
     //Cria o icone de lixeira 
+
+
+
+    //Adicionando na tela
+
+    div.dataset.id = item.id
+    div.appendChild(spanAtividade);
+    div.appendChild(criaBotaoIcone(item.id, div));
+
+    conteudoAtividades.appendChild(div)
+};
+
+
+//Apagando elementos da lista
+function criaBotaoIcone(id,tag){
 
     var spanIcone = document.createElement("span");
     spanIcone.classList.add("icone");
     spanIcone.innerHTML = `<img  src="lixeira-icon.png" alt="Lixeira de reciclagem">`;
 
-    div.appendChild(spanAtividade);
-    div.appendChild(spanIcone);
+    var id = id;
+    var tag = tag
+    spanIcone.addEventListener("click", function(){
+        deletarElemento(tag, id)
+    })
 
-    return div;
+    return spanIcone
+}
 
-};
+function deletarElemento(tag, id){
 
+    tag.classList.add("desaparecer")
 
-//Apagando elementos da lista
-
-IconeLixeira.forEach(
-    () => {
-        conteudoAtividades.addEventListener("click", (event) => {
-                    if(event.target.tagName == "IMG"){
-                        event.target.parentNode.parentNode.classList.add("desaparecer")
-                        setTimeout(
-                            ()=>{
-                                event.target.parentNode.parentNode.remove()
-                            },500)
-                        
-                    }         
-        })
-    }
-)
+    setTimeout( () => {
+        tag.remove()
+        itens.splice(itens.find( elemento => elemento.id === id),1)
+        localStorage.setItem("itens" , JSON.stringify(itens))
+    },500)
+        
+}
